bc074d44c141c26112c589a672b03a168baf4264
anatolyev <anatolyev@yandexlyceum.ru>
2021-12-06 18:19:03 +0300

    Создали код главный

---
+++ main.py
@@ -0,0 +1,13 @@
+def my_suprfunction():
+    print('What an awesome print!')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_suprfunction()
+
+if __name__ == '__main__':
+    main()
